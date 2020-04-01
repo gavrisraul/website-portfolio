544b9d8a87b5d7d1b6f07ba8362199f577f83224
Raul Gavriș <rg.raulgavris@gmail.com>
2020-04-01 22:09:12 +0300

    Added last build

---
+++ frontend/build/precache-manifest.d6b9005c3becc84f23b3620d00e39fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11eb1c78d7706d1224366cac3c93ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7a900b1c10b132721c",
+    "url": "/static/css/2.19b5f3cd.chunk.css"
+  },
+  {
+    "revision": "9887d5b5eb770e56655d",
+    "url": "/static/css/main.161dc9c9.chunk.css"
+  },
+  {
+    "revision": "ac7a900b1c10b132721c",
+    "url": "/static/js/2.3eb7590e.chunk.js"
+  },
+  {
+    "revision": "67b6c398bfc2e9c16a0761be1c3828f0",
+    "url": "/static/js/2.3eb7590e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9887d5b5eb770e56655d",
+    "url": "/static/js/main.2132b6b1.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
